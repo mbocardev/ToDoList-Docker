29a5f6e2909bae3e4ff559c256319fa8b1c43cfe
Abdoul-Majid <7j.sur.7j@gmail.com>
2025-05-29 15:38:58 +0000

    Le code du projet

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'tododb'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connecté à MySQL');
+});
+
+app.get('/tasks', (req, res) => {
+  db.query('SELECT * FROM tasks', (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+app.post('/tasks', (req, res) => {
+  const { title } = req.body;
+  db.query('INSERT INTO tasks (title) VALUES (?)', [title], (err, result) => {
+    if (err) throw err;
+    res.json({ id: result.insertId, title });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(Serveur lancé sur http://localhost:${PORT});
+});
